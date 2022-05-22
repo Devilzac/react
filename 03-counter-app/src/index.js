11b696d2138255f267cb29fa44a70e2a8f381132
Devilzac <shane199274@gmail.com>
2022-05-22 20:22:36 +0200

    merge

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,14 +1,29 @@
+//antes 
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PrimeraApp from './PrimeraApp';
 import CounterApp from './CounterApp';
 import './index.css';
 
-/*
 const saludo = <h1>Hola mundo</h1>;
 
 console.log(saludo)
-*/
+
 const divRoot = document.querySelector("#root");
 
 ReactDOM.render(  <CounterApp value={123} />, divRoot);
+
+*/
+
+
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import CounterApp from './CounterApp';
+import './index.css';
+
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+
+root.render(  <CounterApp value={123} />);
