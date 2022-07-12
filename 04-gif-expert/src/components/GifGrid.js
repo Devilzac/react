129ea6315d7ee19126afd70db36eed42915b00eb
Devilzac <shane199274@gmail.com>
2022-07-12 09:05:38 +0200

    isloading 87

--- 04-gif-expert/src/components/GifGrid.js
+++ 04-gif-expert/src/components/GifGrid.js
@@ -5,11 +5,12 @@ import { getGifs } from '../helpers/getGifs';
 export default function GifGrid({category}) {
 
   const [images, setImages]= useState([]);
-  const [is, setImages]= useState([]);
+  const [isLoading, setIsLoading]= useState(true);
 
 const getImages = async()=>{
   const newImages = await getGifs(category);
   setImages(newImages)
+  setIsLoading(false)
 }
 
 
@@ -21,7 +22,10 @@ useEffect( ()=>{
 
   return (
     <>
-        <h3>{ category }</h3>
+      <h3>{ category }</h3>
+      {
+        isLoading && (<h1>cargando...</h1>) 
+      }
         <div className='card-grid'>
           {images.map( (images) =>(
             <GifGridItem key={images.id} 
