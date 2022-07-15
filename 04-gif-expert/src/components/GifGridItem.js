a6932644f9d9e817d510bfae3fb39b8652c357f8
Devilzac <shane199274@gmail.com>
2022-07-15 10:52:06 +0200

    :)

--- 04-gif-expert/src/components/GifGridItem.js
+++ 04-gif-expert/src/components/GifGridItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const GifGridItem = ({title, url,id}) => {
   return (
     <div className="card">
-        <img src={url} alt={title}/>  
+        <img width="400" height="200" src={url} alt={title}/>  
         <p>{title}</p>
     </div>
 
---
+++ 04-gif-expert/src/components/index.js
@@ -0,0 +1,4 @@
+export * from './AddCategory';
+export * from './GifGrid';
+export * from './GifExpertApp';
+export * from './GifGridItem';
