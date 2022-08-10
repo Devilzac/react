df76a8586d52db493b512fc1c7aa1fa5ba6095a5
Devilzac <shane199274@gmail.com>
2022-08-10 09:09:40 +0200

    118 useEfect Simpleform 118

---
+++ 05-hook-app/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const SimpleForm = () => {
+  return (
+    <>
+        <h1>FS</h1>
+        <hr />
+        <input type="text" className="form-control" placeholder="Username" name="username" />
+    </>
+  )
+}
--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CounterApp } from './01-useState/CounterApp';
+import { SimpleForm } from './02-useEffect/SimpleForm';
+//import { CounterApp } from './01-useState/CounterApp';
+//import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 import './index.css';
 //import { HooksApp } from "./HooksApp";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CounterApp />
+    <SimpleForm />
   </React.StrictMode>
 );
