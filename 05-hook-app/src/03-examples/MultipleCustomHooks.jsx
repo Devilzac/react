4b6511b3c669c1686472e1f86c425b49bc0c6fc7
SupportWallbox <academy.documentation@wallbox.com>
2022-09-07 09:32:25 +0200

    multiple hook 126

---
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useCounter } from '../hooks/useCounter';
+import { useFetch } from '../hooks/useFetch'
+
+export const MultipleCustomHooks = () => {
+    
+    const {counter, increment, decrement, reset} = useCounter(1);
+    const {data, isLoading, hasError} = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+    const {author,quote} = !!data && data[0];
+    const decrementControl = ()=>{
+        if(counter>=2){
+            decrement(1);
+        }
+        else{
+            reset()
+        }
+    }
+    return (
+        <>
+            <h1>Breaking bad Quotes</h1>
+            <hr />
+            {
+                isLoading ? (
+                    <div className='alert alert-info text-center'>
+                        Loading...
+                    </div>
+                ) :
+                (
+                    <blockquote className='blockquote text-end'>
+                        <p className='mb-1'>{quote}</p>
+                        <footer className='blockquote-footer'>{author}</footer>
+                    </blockquote>
+                )
+            }
+            <button className='btn btn-primary m-1' disabled={isLoading} onClick={()=>increment(1)}>
+                Next Quote
+            </button>
+            
+            <button className='btn btn-primary m-1' disabled={isLoading} onClick={decrementControl}>
+                Previous Quote
+            </button>
+        </>
+  )
+}
--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -4,9 +4,10 @@ import ReactDOM from 'react-dom/client';
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 import './index.css';
 //import { HooksApp } from "./HooksApp";
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+//import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <FormWithCustomHook />
+    <MultipleCustomHooks />
 );
