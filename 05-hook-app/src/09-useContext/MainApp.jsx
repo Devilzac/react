188478e1cb248a11e5b1fc38752e917141555aff
SupportWallbox <academy.documentation@wallbox.com>
2022-09-27 09:24:35 +0200

    router & Link

--- 05-hook-app/src/09-useContext/MainApp.jsx
+++ 05-hook-app/src/09-useContext/MainApp.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Routes,Route, Navigate, Link } from "react-router-dom";
+import { HomePage } from './HomePage';
+import { AboutPage } from './AboutPage';
+import { LoginPage } from './LoginPage';
+import { Navbar } from './Navbar';
 
 export const MainApp = () => {
   return (
     <>
     <h1>Main App</h1>
+    
+    <Navbar />
     <hr />
 
+    {/*<Link to="/">Home</Link>
+    <Link to="/login">Login</Link>
+  <Link to="/about">About</Link>*/}
+
+    <Routes>
+      <Route path="/" element={ <HomePage /> } />
+      <Route path="/login/" element={ <LoginPage /> } />
+      <Route path="/about/" element={ <AboutPage /> } />
+
+      {/*<Route path="/*" element={ <LoginPage /> } /> */}
+      <Route path="/*" element={ <Navigate to="/about" /> } />
+    </Routes>
     </>
   )
 }
---
+++ 05-hook-app/src/09-useContext/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link, NavLink } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+            <Link className="navbar-brand" to="/">useContext</Link>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <NavLink className={({isActive})=> `nav-link ${isActive ? 'active':'' }`} to="/login">Login</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className={({isActive})=> `nav-link ${isActive ? 'active':'' }`} to="/about">About</NavLink>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+    </>
+  )
+}
