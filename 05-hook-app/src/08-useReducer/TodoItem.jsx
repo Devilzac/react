7279acbbcf9cc1035aed3bc924308c5aea1642aa
SupportWallbox <academy.documentation@wallbox.com>
2022-11-08 08:24:02 +0100

    pruebas en todoItem

--- 05-hook-app/src/08-useReducer/TodoItem.jsx
+++ 05-hook-app/src/08-useReducer/TodoItem.jsx
@@ -8,8 +8,10 @@ export const TodoItem = ({todo, onDeleteTodo, onToggleTodo}) => {
   return (
     <li className='list-group-item d-flex justify-content-between'>
         <span onClick={()=> onToggleTodo(todo.id)}
+        aria-label='span'
         className={`align-self-center ${(todo.done) ? 'text-decoration-line-through':''}`}>{ todo.description }</span>
-        <button className='btn btn-danger' onClick={ ()=>onDeleteTodo(todo.id) }>Borrar</button>
+        <button
+        aria-label='borrar' className='btn btn-danger' onClick={ ()=>onDeleteTodo(todo.id) }>Borrar</button>
     </li>
   )
 }
--- 05-hook-app/tests/08-useReducer/todoItem.test.jsx
+++ 05-hook-app/tests/08-useReducer/todoItem.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { TodoItem } from '../../src/08-useReducer/TodoItem'
 
 describe('tests en <TodoItem />', () => { 
@@ -25,8 +25,66 @@ describe('tests en <TodoItem />', () => {
         );
 
         const liElement = screen.getByRole('listitem');
-            console.log(liElement);// falta terminarlo
-
+            expect(liElement.className).toBe('list-group-item d-flex justify-content-between');
+        
+        const spanElement = screen.getByLabelText('span');
+        expect(spanElement.className).toContain('align-self-center');
+        
+ 
     }); 
+
+    test('debe de mostrar el Todo completado', () => {
+        
+        todo.done = true;
+
+        render( 
+            <TodoItem  
+                key={todo.id} 
+                todo={todo} 
+                onDeleteTodo={ onDeleteTodoMock } 
+                onToggleTodo={onToggleTodoMock} 
+            />
+        );
+
+         const spanElement = screen.getByLabelText('span');
+        expect(spanElement.className).toContain('text-decoration-line-through');
+         
+    }); 
+
+    test('debe de llamar el ToggleTodo cuando se hace click', () => {
+        
+        render( 
+            <TodoItem  
+                key={todo.id} 
+                todo={todo} 
+                onDeleteTodo={ onDeleteTodoMock } 
+                onToggleTodo={onToggleTodoMock} 
+            />
+        );
+
+        const spanElement = screen.getByLabelText('span');
+        fireEvent.click(spanElement);
+
+        expect( onToggleTodoMock ).toHaveBeenCalledWith( todo.id );
+
+    });
+
+    test('debe de llamar el DeleteTodo cuando se hace click', () => {
+        
+        render( 
+            <TodoItem  
+                key={todo.id} 
+                todo={todo} 
+                onDeleteTodo={ onDeleteTodoMock } 
+                onToggleTodo={onToggleTodoMock} 
+            />
+        );
+        const borrarElement = screen.getByLabelText('borrar');
+        fireEvent.click(borrarElement);
+        expect( onDeleteTodoMock ).toHaveBeenCalledWith( todo.id );
+
+
+    });
+    
 
  });
