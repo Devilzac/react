1fcaf4a53b92baaa50ceb9beb199a4bdb9e25c9f
Devilzac <shane199274@gmail.com>
2023-11-12 15:33:00 -0600

    Axios petition

--- toolkit-redux/src/PokemonApp.jsx
+++ toolkit-redux/src/PokemonApp.jsx
@@ -1,10 +1,16 @@
 import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { getPokemons } from "./store/slices/pokemon/thunks";
 
 export const PokemonApp = () => {
 
 const dispatch = useDispatch();
+
+const {isLoading, pokemons, page} = useSelector( (state) => {
+  return state.pokemons;
+} )
+
+console.log("state", pokemons)
 
 useEffect(() => {
   dispatch(getPokemons());
@@ -16,14 +22,17 @@ useEffect(() => {
     <>
     <h1>PokemonApp</h1>
     <hr />
+    <span>Loading: {  isLoading ? 'True' : 'False'} </span>
     <ul>
-        <li>Hola</li>
-        <li>Hola</li>
-        <li>Hola</li>
-        <li>Hola</li>
-        <li>Hola</li>
+      { pokemons.map(({name, url}) => (
+        <li key={name}>{name}</li>
+        ))
+      }
     </ul>
-    
+<button 
+   disabled={isLoading}
+   onClick={ ()=> dispatch(getPokemons(page))}
+  >Next</button>
     </>
   )
 }
