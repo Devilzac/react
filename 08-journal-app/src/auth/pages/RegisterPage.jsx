79238256d5bacc031688bc29efb15a825f5a102d
Devilzac <shane199274@gmail.com>
2023-12-13 14:54:14 -0600

    show firebase error

--- 08-journal-app/src/auth/pages/RegisterPage.jsx
+++ 08-journal-app/src/auth/pages/RegisterPage.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useMemo, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link as RouterLink } from "react-router-dom"
-import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { Alert, Button, Grid, Link, TextField, Typography } from "@mui/material"
 import { AuthLayout } from "../layout/AuthLayout"
 import { ArrowBack } from "@mui/icons-material";
 import { useForm } from "../../hooks/useForm";
 import { startCreatingUserWithEmailPassword } from "../../store/auth";
+
+
 const formData = {
   displayName: "",
   email: "",
@@ -20,7 +22,11 @@ const formValidations = {
 export const RegisterPage = () => {  
 
   const dispatch = useDispatch();
-  const [formSubmitted, setFormSubmitted] = useState(false)
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const {status, errorMessage} = useSelector(state=>state.auth);
+  const isCheckingAuthentication = useMemo(()=> status==='checking' , [status])
+
 const {formState, displayName, email, password, onInputChange ,
        isFormValid, displayNameValid, emailValid, passwordValid } = useForm(formData, formValidations);
 
@@ -46,6 +52,7 @@ const onSubmit = (event) => {
                   onChange={onInputChange}
                   error={!!displayNameValid && formSubmitted}
                   helperText={displayNameValid}
+                  disabled={isCheckingAuthentication}
                   />
               </Grid>
               <Grid item xs={12} sx={{ mt:2 }}>
@@ -59,6 +66,7 @@ const onSubmit = (event) => {
                   onChange={onInputChange}
                   error={!!emailValid && formSubmitted}
                   helperText={emailValid}
+                  disabled={isCheckingAuthentication}
                    />
               </Grid>
 
@@ -72,12 +80,21 @@ const onSubmit = (event) => {
                   value={password}
                   onChange={onInputChange} 
                   error={!!passwordValid && formSubmitted}
-                  helperText={passwordValid}/>
+                  helperText={passwordValid}
+                  disabled={isCheckingAuthentication}
+                  />
               </Grid>            
 
               <Grid container spacing={ 2 } sx={{ mb:2, mt: 1 }}>
+                <Grid item xs={12}
+                display={!!errorMessage ? '':'none'}>               
+                  <Alert severity="error">
+                      { errorMessage }
+                  </Alert>
+                </Grid>  
                 <Grid item xs={12}>               
-                  <Button type="submit" variant="contained" fullWidth>
+                  <Button type="submit" variant="contained" fullWidth 
+                  disabled={isCheckingAuthentication}>
                       Submit
                   </Button>
                 </Grid>            
