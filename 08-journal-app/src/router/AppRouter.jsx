684a2fbc642a76dbea15d0454cd9b4e0bd394c83
Devilzac <shane199274@gmail.com>
2023-12-29 09:20:15 -0600

    remember login + route

--- 08-journal-app/src/router/AppRouter.jsx
+++ 08-journal-app/src/router/AppRouter.jsx
@@ -1,18 +1,34 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { JournalApp } from "../JournalApp";
 import { AuthRoutes } from "../auth";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout } from "../store/auth";
+import { CheckingAuth } from "../ui/";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { FirebaseAuth } from "../firebase/config";
+import { useCheckAuth } from "../hooks/useCheckAuth";
 
 export const AppRouter = () => {
+
+  const {status} = useCheckAuth();
+  if (status==='checking') {
+    return <CheckingAuth/>
+  }
+  
   return (
     <Routes>
-        <Route>
+      {
+      (status==='authenticated') 
+      ? <Route path="/*" element={ <JournalRoutes/> } />
+      : <Route path="/auth/*" element={ <AuthRoutes/> } />
+      
+      }      
+      <Route path="/*" element={ <Navigate to='/auth/login' />} />
             {/* Login */}
-            <Route path="/auth/*" element={ <AuthRoutes/> } />
 
             {/* Journal App */}
-            <Route path="/*" element={ <JournalRoutes/> } />
-        </Route>
     </Routes>
   )
 }
